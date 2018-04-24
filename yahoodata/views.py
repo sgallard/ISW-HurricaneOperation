6c74e12eb0949b3f8e5aa54ae0a2d52c7dec049b
Bastian Quezada <bquezada@alumnos.inf.utfsm.cl>
2018-04-23 21:51:32 -0300

    Using R

--- yahoodata/views.py
+++ yahoodata/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from datetime import date
-
+from datetime import date, datetime
+import rpy2
+import rpy2.robjects as robjects
+import numpy as np
 from yahoo_quote_download import yqd
 
 # Create your views here.
@@ -34,8 +36,45 @@ def getdata(request):
     for x in historical_data_raw[1::]:
         historical_data.append(x.split(','))
 
-    context = {"historical_data": historical_data, "header_tabla": historical_data_raw[0].split(','),
-    "accion": codigo
-    }
+  
+    adj = []
+    largo = len(historical_data)
+    for i in range(largo-1):
+        adj.append(historical_data[i][5])
+    adj = [float(i) for i in adj]
+    compra= datetime.strptime(req.get("fecha_compra"), '%Y-%m-%d').date()
+    hoy=date.today()    
+    Tm = compra - hoy
+    Tm=Tm.days/365.
+    S = float(historical_data[-2][5])
+    k = np.mean(adj)
+    sigma = np.std(adj)
+    r=float(req.get("tasa_interes"))/100.
+    print("S= ",S)
+    print("k= ",k)
+    print("Tm= ",Tm)
+    print("r= ",r)
+    print("sigma= ",sigma)
+    r_f = robjects.r('''black_scholes <- function(S, k, Tm, r, sigma){
+        values <- c(1)
+        d1 <- (log(S/k) + (r+(sigma^2)/2)*(Tm))/(sigma*sqrt(Tm))
+        d2 <- (log(S/k) + (r-(sigma^2)/2)*(Tm))/(sigma*sqrt(Tm))
+        
+        values[1] <- S*pnorm(d1) - k*exp(-r*Tm)*pnorm(d2)
+        values[2] <- k*exp(-r*Tm)*pnorm(-d2)- S*pnorm(-d1)
 
+        values
+    }''')
+    res= r_f(S,k,Tm,r,sigma)
+    compra=res[0]
+    venta=res[1]
+    
+    if req.get("tipo")=="Compra":
+        context = {"historical_data": historical_data, "header_tabla": historical_data_raw[0].split(','),
+        "accion": codigo, "resultado": compra,"tipo": "comprar", "fecha":req.get("fecha_compra")
+        }
+    else:
+        context = {"historical_data": historical_data, "header_tabla": historical_data_raw[0].split(','),
+        "accion": codigo, "resultado": venta, "tipo": "vender", "fecha":req.get("fecha_compra")
+        }
     return render(request,'yahoodata/viewResults.html',context)
